8a2fcfdb652752def4fbf59785ccbbf1aa2d0b2e
narvis2 <narvis2@douzone.com>
2022-12-10 14:08:14 +0900

    🍎 Project Init 🍎
    - 프로젝트 생성

---
+++ CovidMVVM/CovidMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidMVVMApp.swift
+//  CovidMVVM
+//
+//  Created by choi young jun on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct CovidMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
